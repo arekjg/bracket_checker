1ddb62ed802dcee44743f509c257aad35243df3b
arekjg <arekjg@gmail.com>
2022-10-15 22:32:23 +0200

    changed unordered set and pairs with two arrays of chars, finished checkPairs function and main function. There are some bugs to fix and further testing

--- bracket_checker.cpp
+++ bracket_checker.cpp
@@ -1,46 +1,82 @@
 #include <iostream>
 #include <stack>
 #include <string>
-#include <utility>
-#include <unordered_set>
 
 using namespace std;
 
-//bool checkPair(char bracket1, char bracket2)
-//{
-//
-//}
-//
+
+bool checkPair(char bracket1, char bracket2)
+{
+	// Initializing char arrays with open brackets and close brackets
+	char brackets_open[4] =	 { '(', '{', '[', '<' };
+	char brackets_close[4] = { ')', '}', ']', '>' };
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (bracket1 == brackets_open[i] && bracket2 == brackets_close[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
 int main()
 {
+	// Initializing stack and user input
 	stack<char> brackets;
 	string input;
-	unordered_set<pair<char, char>> bracket_pairs;
-	//bracket_pairs.insert("(", ")");
-	//bracket_pairs.insert("{", "}");
-	//bracket_pairs.insert("[", "]");
-	//bracket_pairs.insert("<", ">");
 
-	// hash function to insert pairs to unordered set
 
 	cout << "Welcome to bracket checker!\n";
 	cout << "It checks if given brackets are in correct order.\n";
-	cout << "Valid brackets include : () { } [] < >.Every other character will be ignored.\n";
-	cout << "Insert brackets: ";
+	cout << "Valid brackets include : () {} [] <>. Every other character will be ignored.\n";
+	cout << "Insert brackets in one line: ";
+	cin >> input;
 
-	//for (auto x : bracket_pairs)
-	//{
-	//	cout << x.first << x.second << endl;
-	//}
+	/*
+	for (int i = 0; i < input.length(); i++)
+	{
+		brackets.push(input[i]);
+		cout << brackets.top() << "\n";
+		if (i > 0)
+		{
+			if (checkPair)
+			{
+				cout << "True";
+			}
+			else
+			{
+				cout << "False";
+			}
+		}
+	}
+	*/
 
-	//cin >> input;
 
-	//for (char i = 0; i < input.length(); i++)
-	//{
-	//	brackets.push(input[i]);
-	//	cout << brackets.top() << "\n";
-	//}
+	// Push first bracket from user's input to stack
+	brackets.push(input[0]);
+
+	for (char i = 1; i < input.length(); i++)
+	{
+		if (checkPair(brackets.top(), input[i]))
+		{
+			brackets.pop();
+		}
+		else
+		{
+			brackets.push(input[i]);
+		}
+	}
+
+	if (brackets.empty())
+	{
+		cout << "Valid!";
+	}
+	else
+	{
+		cout << "Invalid!";
+	}
 
 
 }
@@ -48,8 +84,11 @@ int main()
 // goal of the app is to check if brackets in array are in correct order and if they are all closed
 
 // TODO:
-// iterate through the string and push every char to stack
+// iterate through the string and push every char to stack	- DONE
 // after each iteratiorn check if first and second items are matching brackets:
 //		if yes, pop them from the stack
 //		if no, continue
 // after every succesfull pop there should be another check of two first items in stack
+
+
+// problem to fix: when first and second brackets are matching the error pops up, because the stack is empty in this moment
